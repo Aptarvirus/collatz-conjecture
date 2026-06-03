856c03b520356670a3b000062078ca0a69b1e738
Aptar Hussain <192695585+Aptarvirus@users.noreply.github.com>
2026-06-03 17:25:49 +0530

    Add files via upload

---
+++ collatz.py
@@ -0,0 +1,39 @@
+##### the collatz conjecture#####
+## the unsloved problem 
+
+
+#Try is used so that if something goes wrong, do not crash automatically(((it is kinda safty net)))
+
+
+try:
+    # choice = "yes"
+    # while choice =="yes":
+        number = int(input("Enter any number"))
+    #  above line is for taking input from a user
+        if number <= 0 :
+            print("enter  a positive number, other then one  ")
+          #above line is a condition that if  the given number is less than equal
+          #then, the give line will print 
+          #  enter a numm.....
+        elif number <= 1:
+           print("Enter any other number 1 :") # if the user enters 1 then the given statment will print
+        else:
+            while number != 1:
+                if number %2 == 0:
+                    number = number //2 #to find the floor division
+#above and below line are for the collatz  conjection         
+                  
+                  # tis is the formula by which we use to find whether the number is odd or even
+                else:
+                  number = 3* number +1 # it will multiply the number by 3 and add the multiplied number by 1
+                print(number)
+except ValueError: # to avoid the value error 
+    #  choice = input("Do you want to continue? (yes/no): ")
+    print("U have entered wrong value plz enter a correct one") #if the given number is wrong then it will print the above statement
+     
+    
+    
+    
+    
+
+        
---
+++ readme.md
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+  
+## Badges  
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/)  
+[![GPLv3 License](https://img.shields.io/badge/License-GPL%20v3-yellow.svg)](https://choosealicense.com/licenses/gpl-3.0/)  
+[![AGPL License](https://img.shields.io/badge/license-AGPL-blue.svg)](https://choosealicense.com/licenses/gpl-3.0/)  
+
+# Collatz Conjecture Visualizer
+### This is a simple Python implementation of the Collatz Conjecture also know as (3n +1 problem)
+## How It works 
+## Collatz Conjecture 
+### follows two simple rules pf any positive integer n:
+
+### * If n is is even: divide it by 2(n/2)
+### * If n is odd : multiply it by 3 and add 1(3n+1)
+
+# Features
+## Input Validation:
+### Use try-except blokc to handle non integer inputs. preventing the program from crashing
+## Error Handling: 
+### Checks for numbers less than or equal to $0$ or the number $1$ to ensure valid operation.
+
+# Demo 
+### 1. Run the Script in ur terminal or in th e IDE
+### 2. Enter any posittive integer other then 1
+### python collatz.py (Run in your cmd)
